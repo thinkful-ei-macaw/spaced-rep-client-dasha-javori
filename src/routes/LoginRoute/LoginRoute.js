ac2b7fa20b1d7f3ff79a6b61325f380fe2b5cf9c
dasha akhten <dasha.akhten@gmail.com>
2020-05-18 09:37:04 -0700

    an attempt was made

--- src/routes/LoginRoute/LoginRoute.js
+++ src/routes/LoginRoute/LoginRoute.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react'
-import LoginForm from '../../components/LoginForm/LoginForm'
+import React, { Component } from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import "./LoginRoute.css";
 
 class LoginRoute extends Component {
   static defaultProps = {
     location: {},
     history: {
-      push: () => { },
+      push: () => {},
     },
-  }
+  };
 
   handleLoginSuccess = () => {
-    const { location, history } = this.props
-    const destination = (location.state || {}).from || '/'
-    history.push(destination)
-  }
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || "/";
+    history.push(destination);
+  };
 
   render() {
     return (
-      <section>
-        <h2>Login</h2>
-        <LoginForm
-          onLoginSuccess={this.handleLoginSuccess}
-        />
-      </section>
+      <div className="login-wrapper">
+        <section>
+          <h2>Login</h2>
+          <LoginForm onLoginSuccess={this.handleLoginSuccess} />
+        </section>
+      </div>
     );
   }
 }
 
-export default LoginRoute
+export default LoginRoute;
--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import React, { Component } from "react";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import "./RegistrationRoute.css";
 
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
       push: () => {},
     },
-  }
+  };
 
   handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
-  }
+    const { history } = this.props;
+    history.push("/login");
+  };
 
   render() {
     return (
-      <section>
-        <p>
-          Practice learning a language with the spaced reptition revision technique.
-        </p>
-        <h2>Sign up</h2>
-        <RegistrationForm
-          onRegistrationSuccess={this.handleRegistrationSuccess}
-        />
-      </section>
+      <div className="reg-wrapper">
+        <section>
+          <p>
+            Practice learning a language with the spaced reptition revision
+            technique.
+          </p>
+          <h2>Sign up</h2>
+          <RegistrationForm
+            onRegistrationSuccess={this.handleRegistrationSuccess}
+          />
+        </section>
+      </div>
     );
   }
 }
 
-export default RegistrationRoute
+export default RegistrationRoute;
